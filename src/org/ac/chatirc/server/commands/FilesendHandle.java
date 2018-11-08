51f828de25ccbef938f4746f8b2d3b134dc1bc5f
HMhugomeireles <meireles.hmm@gmail.com>
2018-11-08 09:47:35 +0000

    add new comand file send only the Handle

---
+++ src/org/ac/chatirc/server/commands/FilesendHandle.java
@@ -0,0 +1,13 @@
+package org.ac.chatirc.server.commands;
+
+import org.ac.chatirc.server.Server;
+import org.ac.chatirc.server.User;
+
+public class FilesendHandle implements CommandHandle {
+
+    @Override
+    public void dispatch(Server server, User user, String line) {
+
+    }
+
+}
